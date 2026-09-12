ae8df5f84c4d9fecbe71e1b0249a7b2981058390
ajay <ajay@gmail.com>
2026-09-12 13:52:00 +0530

    ASD

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "vnet1"{
     name = "vnet"
     location = azurerm_resource_group.rg1.location
     resource_group_name = azurerm_resource_group.rg1.name
-    address_space = ["10.0.0.0/16"]
+    address_space = ["10.1.0.0/16"]
 
 }
 
